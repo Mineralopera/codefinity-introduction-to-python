ed0a1de543172eee89499fa7953276f661b426db
Mineralopera <>
2025-11-21 10:08:53 +0000

    Challenge: Pricing Adjustment Capstone

---
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -0,0 +1,42 @@
+# Creating dictionary with grocery items
+grocery_inventory = {
+    "Milk": ("Dairy", 3.50, 8),
+    "Eggs": ("Dairy", 5.50, 30),
+    "Bread": ("Bakery", 2.99, 15),
+    "Apples": ("Produce", 1.50, 50)
+}
+
+# Operating Eggs
+price_eggs = grocery_inventory.get("Eggs")[1]
+if price_eggs > 5:
+    print("Eggs are too expensive, reducing the price by $1.")
+    grocery_inventory["Eggs"] = ("Dairy", 4.50, 30)
+    print(f"New egg price: {grocery_inventory["Eggs"][1]}")
+
+else:
+    print("The price of Eggs is reasonable.")
+
+
+# Adding a New Item
+grocery_inventory.update({"Tomatoes": ("Produce", 1.20, 30)})
+print(f"Inventory after adding Tomatoes: {grocery_inventory}")
+
+# Managing stock of milk
+milk_stock = grocery_inventory.get("Milk")[2] 
+if milk_stock < 10:
+    print("Milk needs to be restocked. Increasing stock by 20 units.")
+    grocery_inventory["Milk"] = ("Dairy", 3.50, 28)
+    print(f"New stock of milk: {grocery_inventory["Milk"][2]}")
+else:
+    print("Milk has sufficient stock.")
+
+# Managing Apples
+apple_price = grocery_inventory.get("Apples")[1]
+if apple_price > 2:
+    grocery_inventory.pop("Apples")
+    print("Apples removed from inventory due to high price.")
+
+# Print updated inventory
+print(f"Updated inventory: {grocery_inventory}")
+    
+
