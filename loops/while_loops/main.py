b832b566b9ac0ceb4f35e4da0ffd245eeead2011
Mineralopera <>
2025-11-21 16:48:26 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,17 @@
+start_number = 5
+countdown_values = []
+
+# Adding start number to countdown
+countdown_values.append(start_number)
+
+# Creating countdown
+while start_number > 1:
+    start_number -= 1
+    countdown_values.append(start_number)
+
+# Printing message
+print("Discount countdown complete!")
+print(countdown_values)
+    
+ 
+
