f7205596dd1f2ad0c98426d8a11242a5585ad222
Mineralopera <>
2025-11-22 15:44:32 +0000

    Nested loops

---
+++ loops/nested_loops/main.py
@@ -0,0 +1,12 @@
+produce = ["Tomatoes", "Lettuce"]
+dairy = ["Milk", "Cheese"]
+
+# Combining list
+groceries = [produce, dairy]
+
+for section in groceries:
+    for item in section:
+        print(item)
+
+
+
